2844736ce19e62b5b9ec67c9c7e49110bdb6d1b1
Mayuri <Mayuri@Mayu>
2015-02-02 16:03:46 +0530

    init

---
+++ src/newpythonproject.py
@@ -0,0 +1,24 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Prakash Khandelwal"
+__date__ = "$Feb 1, 2015 2:24:10 PM$"
+
+from nltk import *
+
+def Tokenization(sent1):
+    list1=word_tokenize(sent1)
+    print list1
+    i=0
+    while(list1[i]!=0):
+        StemmingFunc(list1[i])
+        i+=1
+
+def StemmingFunc(word1):
+    wnl=WordNetLemmatizer()
+    stemWord=wnl.lemmatize(word1)
+    print stemWord
+           
+Tokenization(input("enter sent: "))
+
